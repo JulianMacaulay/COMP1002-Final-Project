b744380ebbb8a372dc9400fed3b0eec2772faf12
JouChun <aafree2014@gmail.com>
2024-03-22 16:05:10 -0400

    update navbar

--- header.js
+++ header.js
@@ -1,15 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var header = document.createElement('header');
-    header.innerHTML = `
+document.addEventListener("DOMContentLoaded", function () {
+  var header = document.createElement("header");
+  header.innerHTML = `
         <nav>
             <ul class="header-navigation">
                 <li class="logo_item"><img src="images/3DO_Company_logo.png" alt="3DO Logo" class="logo"></li>
-                <li><a href="#shop">Shop</a></li>
-                <li><a href="#about">About Company</a></li>
-                <li><a href="#contact">Contact US</a></li>
-                <li><a href="#services">Services</a></li>
+                <li>
+                    <ul>
+                    <li><a href="#shop">Shop</a></li>
+                <li><a href="./About.html">About Us</a></li>
+                <li><a href="./Contact.html">Contact US</a></li>
+                
+                </ul>
+                </li>
             </ul>
         </nav>
     `;
-    document.body.insertBefore(header, document.body.firstChild);
+  document.body.insertBefore(header, document.body.firstChild);
 });
